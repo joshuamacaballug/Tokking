050a48c43c8c486aa993743ab59c5a50e0fd7fbb
Josh <jmacaballug05@gmail.com>
2024-06-03 01:42:35 +0400

    Deployment

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
+
+const MessageContainer = () => {
+	const { selectedConversation, setSelectedConversation } = useConversation();
+
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+
+	return (
+		<div className='md:min-w-[450px] flex flex-col backdrop-blur-3xl bg-yellow-400 bg-opacity-30'>
+			{!selectedConversation ? (
+				<NoChatSelected />
+			) : (
+				<>
+					{/* Header */}
+					<div className='bg-yellow-300 px-4 py-2 mb-2'>
+						<span className='label-text text-black'>To:</span>{" "}
+						<span className='text-black font-bold'>{selectedConversation.username}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
+			)}
+		</div>
+	);
+};
+export default MessageContainer;
+
+const NoChatSelected = () => {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome, {authUser.username}!👋</p>
+				<p>Select a user to start Tokking!</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
+};
+
+// STARTER CODE SNIPPET
+// import MessageInput from "./MessageInput";
+// import Messages from "./Messages";
+
+// const MessageContainer = () => {
+// 	return (
+// 		<div className='md:min-w-[450px] flex flex-col'>
+// 			<>
+// 				{/* Header */}
+// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
+// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+// 				</div>
+
+// 				<Messages />
+// 				<MessageInput />
+// 			</>
+// 		</div>
+// 	);
+// };
+// export default MessageContainer;
